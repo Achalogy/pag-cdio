1654ee46344885a0bee9bfe7907c76845b32ac98
Achalogy <achalogy@gmail.com>
2024-04-20 19:49:39 -0500

    Nueva pagina

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,28 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-
+		extend: {
+			textShadow: {
+				sm: '0 1px 1px black',
+				DEFAULT: '0 2px 4px black',
+				lg: '0 8px 16px black',
+			},
+		},
 	},
-	plugins: [],
+	plugins: [
+		plugin(function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					'text-shadow': (value) => ({
+						textShadow: value,
+					}),
+				},
+				{ values: theme('textShadow') }
+			)
+		}),
+	],
 	darkMode: "class"
 }
